c90e09061b2c11e3171f0c3826a82f911c8a0c26
lzil <liang.zhou.18@ucl.ac.uk>
2022-02-20 20:14:12 +0000

    everything but leaderboard working again. unfortunately info on captured generals no longer avail

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 // GENERALS.IO TOOLS
 
+// current list of colors
 var COLORS = {
-	'blue': '#0000ff', 'red': '#ff0000',
-	'orange': '#ffa500', 'darkgreen': '#004600',
-	'teal': '#008080', 'purple': '#800080',
-	'green': '#008000', 'maroon': '#800000'
+	'red': 'red', 'green': 'green',
+	'lightblue': '#4363d8', 'purple': 'purple',
+	'teal': 'teal', 'blue': 'blue',
+	'orange': '#f58231', 'maroon': '#800000',
+	'yellow': '#b09f30', 'pink': '#f032e6',
+	'brown': '#9a6324', 'lightgreen': '7ab78c',
+	'purple-blue': '#483d8b'
 }
 
 var yourColor = null;
@@ -13,21 +17,22 @@ var tileObserver = new MutationObserver(function(mutations) {
 	mutations.forEach(function(mutation) {
 		var elt = mutation.target
 		var props = elt.className.split(' ')
-		//console.log(elt)
 
 		// give 'mountain' type once a mountain is seen
 		if (props.indexOf('mountain') !== -1) {
 			elt.type = 'mountain'
+			elt.style.opacity = 1
 		}
 
 		// once something is a mountain it is permanently a mountain
+		// if server changes it back to foggy, make it not foggy
 		if (props.indexOf('fog') !== -1 && elt.type === 'mountain') {
 			elt.className = elt.className.replace(/\bfog\b/,'mountain')
 			elt.className = elt.className.replace(/\bobstacle\b/,'')
 		}
 
 		// draw a white border around seen generals
-		if (props.indexOf('general') !== -1) {
+		if (props.indexOf('general') !== -1 && elt.type !== 'general') {
 			elt.type = 'general'
 			if (!yourColor) {
 				yourColor = props[0]
@@ -43,45 +48,31 @@ var tileObserver = new MutationObserver(function(mutations) {
 
 		// give 'city' type once a city is seen
 		if (props.indexOf('city') !== -1) {
-			
-			if (elt.type === 'unexplored' || elt.type === 'general') {
+			if (elt.type == 'unexplored' || elt.type == 'general') {
 				// this was a past general!
 				elt.style.border = '1px solid white'
 				elt.type = 'general'
-			} else {
-				// just a city
+			} else if (props[0].length == 0) {
+				// city owned by nobody
 				elt.style.border = '1px dashed white'
 				elt.type = 'city'
+			} else {
+				// city owned by somebody
+				elt.style.border = '1px dashed ' + COLORS[props[0]]
+				elt.type = 'city'
 			}
-			
-			// only give a border when you can't see the city
-			// if (props.indexOf('') === -1) {
-			// 	elt.nextBorder = '1px ' + COLORS[props[0]]
-			// } else {
-			// 	elt.nextBorder = '1px white'
-			// }
-
-
 		}
-
-		// once something is a city it is permanently a city, possibly with a color
-		// if (elt.type === 'city') {
-		// 	if (props.indexOf('fog') !== -1) {
-		// 		elt.style.border = elt.nextBorder;
-		// 	} else {
-		// 		elt.style.border = '1px solid black'
-		// 	}
-		// }
 
 		// denote area that's been seen
-		if (props.indexOf('neutral') !== -1 || (props.indexOf('fog') === -1)) {
-			if (props.indexOf('attackable') !== -1) {
-				elt.style.opacity = '0.4'
+		if (props.indexOf('fog') === -1) {
+			if (elt.type == 'unexplored') {
 				elt.type = 'explored'
-			} else {
-				elt.style.opacity = '1'
 			}
+			elt.style.opacity = 1
+		} else if (elt.type !== 'mountain') {
+			elt.style.opacity = 0.5
 		}
+
 	});    
 });
 
@@ -136,24 +127,34 @@ var gameObserver = new MutationObserver(function(mutations) {
 
 			setTimeout(function() {
 				console.log('loading map')
-				var map = document.getElementById('map')
+				var map = document.getElementById('gameMap')
 				var tiles = map.getElementsByTagName('td')
 
 				for (var i = 0; i < tiles.length; i++) {
 					var props = tiles[i].className.split(' ')
-					if (props.indexOf('obstacle') === -1) {
+					if (props.indexOf('obstacle') !== -1) {
+						// it's foggy, and either a mountain or a city
+						tiles[i].type = 'obstacle'
+					} else {
 						if (props.indexOf('fog') !== -1) {
+							// it's foggy but not a mountain, so due to explore
 							tiles[i].type = 'unexplored'
-							tiles[i].style.opacity = '0.5'
+							tiles[i].style.opacity = 0.2
 						} else {
+							// it's not foggy
+							tiles[i].style.opacity = 1
 							if (props.indexOf('city') !== -1) {
+								// is a city
 								tiles[i].type = 'city'
+							} else if (props.indexOf('mountain') !== -1) {
+								// is a mountain 
+								tiles[i].type = 'mountain'
+								tiles[i].style.opacity = 1
 							} else {
-								tiles[i].type = 'explored'
+								// is a city
+								tiles[i].type = 'city'
 							}
 						}
-					} else {
-						tiles[i].type = 'obstacle'
 					}
 				}
 
